680171c025cae5f20dfea439c3a9fdb92a565811
christianjason <cornelchristian18@gmail.com>
2025-02-04 21:30:26 +0800

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,6 @@
+// api/products.js
 import express from "express";
 import dotenv from "dotenv";
-import path from "path";
 import { connectDB } from "../config/db.js";
 import productRoutes from "../routes/product.route.js";
 
@@ -14,19 +14,9 @@ app.use(express.json());
 // Routes
 app.use("/api/products", productRoutes);
 
-// Serve frontend files in production
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "frontend", "dist")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-//   });
-// }
-
 // Connect to the database
 connectDB();
 
-// For local development (use app.listen instead of createServer)
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+export default (req, res) => {
+  app(req, res); // Handle the incoming request using Express
+};
