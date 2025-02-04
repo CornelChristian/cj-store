45cb37ab6fe005428ae3e2108499108b07be13d7
christianjason <cornelchristian18@gmail.com>
2025-02-04 21:28:09 +0800

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -15,12 +15,12 @@ app.use(express.json());
 app.use("/api/products", productRoutes);
 
 // Serve frontend files in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend", "dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "frontend", "dist")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+//   });
+// }
 
 // Connect to the database
 connectDB();
